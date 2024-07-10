b356964a7eaf1397373a96ad5edb138cfd82f027
fayasPA <fayasmuthalib@gmail.com>
2024-07-10 17:19:30 +0530

    updated colors in tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl text-green font-bold underline bg-white h-[100vh]">
       Hello world!
     </h1>
   );
