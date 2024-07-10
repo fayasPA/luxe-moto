5ccff450d629e8bfc039a84358cc20f30a6f7973
fayasPA <fayasmuthalib@gmail.com>
2024-07-10 15:58:15 +0530

    env updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-export default function App() {
+function App() {
+
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-  )
+  );
 }
+
+export default App;
