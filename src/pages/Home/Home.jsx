27b633620515af68b7b703ead3ed4b1a581dad72
Kiran <kiran.nj.98@gmail.com>
2024-08-04 15:46:37 +0530

    comiit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import BannerCarousel from '../../components/BannerCarousel'
+import ZoomSlider from './../../components/ZoomSlider';
 const Home = () => {
   return (
     <section className='h-screen w-full'>
       <BannerCarousel />
+      <ZoomSlider />
       <div>fays</div>
       <div>fays</div>
       <div>fays</div>
