9dc9ea9557388f627a592021e2899d642f1187cd
Kiran <kiran.nj.98@gmail.com>
2024-08-20 16:15:59 +0530

    commit

---
+++ src/components/ImageShowcase.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import info from '../../public/images/benz.jpg';
+
+const ImageShowcase = () => {
+  return (
+    <div>
+      <div>
+        <div>
+            <image src={info}/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ImageShowcase
