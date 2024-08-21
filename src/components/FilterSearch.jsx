e838ac2e622d65b642da46b8fbd618476eae86de
fayasPA <fayasmuthalib@gmail.com>
2024-08-21 16:28:09 +0530

    filtersearch update

--- src/components/FilterSearch.jsx
+++ src/components/FilterSearch.jsx
@@ -83,7 +83,7 @@ const FilterSearch = () => {
       {/* Modal */}
       {isModalOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg w-96">
+          <div className="bg-white p-6 rounded-none shadow-lg w-96">
             <div className="flex justify-between items-center mb-4">
               <h2 className="text-xl font-semibold">Filter Options</h2>
               <button onClick={toggleModal}>
@@ -95,32 +95,41 @@ const FilterSearch = () => {
                 <label className="block text-sm font-medium text-gray-700 mb-2">
                   Brand
                 </label>
-                <select className="w-full px-3 py-2 border bg-white border-gray-300 rounded-md focus:border-transparent focus:ring-0">
+                <select
+                  className="w-full border border-gray-300 rounded-none flex flex-col gap-2 p-2 bg-white shadow-xl origin-top focus:outline-none focus:ring-0 focus:border-1"
+                >
                   <option>Brand 1</option>
                   <option>Brand 2</option>
                   <option>Brand 3</option>
                 </select>
               </div>
+
               <div className="mb-4">
                 <label className="block text-sm font-medium text-gray-700 mb-2">
                   Body
                 </label>
-                <select className="w-full px-3 py-2 border bg-white border-gray-300 rounded-md focus:border-transparent focus:ring-0">
+                <select
+                  className="w-full border border-gray-300 rounded-none flex flex-col gap-2 p-2 bg-white shadow-xl origin-top focus:outline-none focus:ring-0 focus:border-1"
+                >
                   <option>Body 1</option>
                   <option>Body 2</option>
                   <option>Body 3</option>
                 </select>
               </div>
+
               <div className="mb-4">
                 <label className="block text-sm font-medium text-gray-700 mb-2">
                   Fuel
                 </label>
-                <select className="w-full px-3 py-2 border bg-white border-gray-300 rounded-md focus:border-transparent focus:ring-0">
+                <select
+                  className="w-full border border-gray-300 rounded-none flex flex-col gap-2 p-2 bg-white shadow-xl origin-top focus:outline-none focus:ring-0 focus:border-1"
+                >
                   <option>Fuel 1</option>
                   <option>Fuel 2</option>
                   <option>Fuel 3</option>
                 </select>
               </div>
+
               <div className="flex justify-end">
                 <button
                   type="button"
