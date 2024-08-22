e8413d90994db99357ef2ed86377e2ee88761362
Kiran <kiran.nj.98@gmail.com>
2024-08-22 15:59:56 +0530

    vehicle page

--- src/pages/VehicleDetails/VehicleDetails.jsx
+++ src/pages/VehicleDetails/VehicleDetails.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import VehicleDetail from '../../components/VehicleDetail'
 
 const VehicleDetails = () => {
   return (
-    <div className='h-screen bg-red text-3xl text-green pt-60'><p className='bg-blue-800'>VehicleDetails</p></div>
+  /*   <div className='h-screen bg-red text-3xl text-green pt-60'><p className='bg-blue-800'>VehicleDetails</p></div> */
+  <div><VehicleDetail/></div>
   )
 }
 
